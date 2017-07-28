d44b26da32b8198dd531229961c856acd6017272
ciazhar <daisetsunakama@gmail.com>
2017-07-28 09:29:02 +0700

    config service w/ 1 app configuration

---
+++ src/main/java/id/clorus/config/service/ConfigServiceApplication.java
@@ -0,0 +1,15 @@
+package id.clorus.config.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServiceApplication.class, args);
+	}
+}
